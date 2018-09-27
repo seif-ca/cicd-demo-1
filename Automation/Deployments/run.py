d953fa61c36b61d2aef83d70bf7d3f9c058c2267
tamisin-sandbox <38165272+tamisin-sandbox@users.noreply.github.com>
2018-09-27 11:29:01 -0400

    Update run.py

--- Automation/Deployments/run.py
+++ Automation/Deployments/run.py
@@ -104,7 +104,7 @@ def run(shard, username, password, notebooks, outfilepath):
         fullworkspacepath = workspacepath + '/' + name
 
         print('Running job for:' + fullworkspacepath)
-        values = {'run_name': name, 'existing_cluster_id': '0410-142436-shoos167', 'timeout_seconds': 3600, 'notebook_task': {'notebook_path': fullworkspacepath}}
+        values = {'run_name': name, 'existing_cluster_id': '0824-005931-heel30', 'timeout_seconds': 3600, 'notebook_task': {'notebook_path': fullworkspacepath}}
 
         resp = requests.post('https://' + shard + '.cloud.databricks.com/api/2.0/jobs/runs/submit',
                              data=json.dumps(values), auth=(username, password))
