4b7bd57146478948845b66a4d33da426bab7fbfd
tamisin-sandbox <38165272+tamisin-sandbox@users.noreply.github.com>
2018-12-21 09:57:46 -0500

    Update: .DS_Store .idea Automation Dbfs ETL Includes ML README.md Training Validation Workspace gitsync.sh h20.py notebooks test.py word2vec.py

--- Workspace/ETL/LoadAllNationsDemo.scala
+++ Workspace/ETL/LoadAllNationsDemo.scala
@@ -33,6 +33,8 @@ val joinedDf = nationsDf.join(regionsDf, nationsDf("RegionKey") === regionsDf("R
 joinedDf.createOrReplaceTempView("V_ALL_NATIONS")
 // this is a test
 // This is a demo
+//again and again
+
 
 // COMMAND ----------
 
