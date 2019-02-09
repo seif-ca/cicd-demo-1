79d211888b761eda8191b8aa8bc7f98b28cb8fbb
tamisin-sandbox <38165272+tamisin-sandbox@users.noreply.github.com>
2019-02-08 20:16:51 -0500

    updated to add DB Connect test

--- Validation/Tests/test.py
+++ Validation/Tests/test.py
@@ -37,6 +37,25 @@ class TestJobOutput(unittest.TestCase):
             statuses.append(status)
 
         self.assertFalse('FAILED' in statuses)
+        
+    def getSimilarProds(self):
+        # connect to shard using dbconnect
+        spark = SparkSession\
+                .builder\
+                .getOrCreate()
+
+        # The Spark code will execute on the Databricks cluster.
+        df = spark.sql("select * from product_similarity")
+
+        dfcnt = df.where(df['similarity_score'] > .5)
+
+        return dfcnt.count()
+
+    def test_SimilarProds(self):
+
+        val = self.getSimilarProds()
+        print(val)
+        self.assertGreater(val, 1, "Bad")        
 
         
 if __name__ == '__main__':
